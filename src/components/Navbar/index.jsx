355e36b077f27e79e03bb9000d0603e39d2ea29f
keldenl <keldenlin@gmail.com>
2022-08-20 15:48:06 -0700

    set up simple project structure

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export function Navbar({ }) {
+    return (
+        <header>
+            <h1>Picar</h1>
+        </header>
+    )
+}
---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Navbar } from '../../components/Navbar';
+import './style.css';
+
+export function Homepage({ }) {
+    return (
+        <div>
+            <Navbar />
+            <p>Homepage</p>
+        </div>
+    )
+}
