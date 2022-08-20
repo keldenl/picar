7c1365fb1104b226d32b4714860cce614e67e77f
keldenl <keldenlin@gmail.com>
2022-08-20 15:55:35 -0700

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Homepage } from './pages/Homepage';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
-    </div>
+    <Router basename="/picar">
+      <div className="App">
+        <Routes>
+          {/* <Route
+            path="/users"
+            element={<Users />}
+          /> */}
+          <Route
+            path="/"
+            element={<Homepage />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import './style.css';
 
 export function Navbar({ }) {
     return (
         <header>
             <h1>Picar</h1>
-        </header>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+            </ul >
+        </header >
     )
 }
