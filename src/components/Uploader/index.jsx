221fff7548baf9a8f8a94b786181a8c57f055c0c
Eliza Huang <eliza.huang@alaskaair.com>
2022-08-20 17:46:16 -0700

    feat: add basic uploading component files

---
+++ src/components/Uploader/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export function Uploader({ }) {
+    return (
+        <div>
+            Choose a photo to upload
+        </div>
+        
+    )
+}
--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../../components/Navbar';
+import { Uploader } from '../../components/Uploader';
 import './style.css';
 
 export function Homepage({ }) {
+    const [showUploader, setShowUploader] = useState(false);
+
     return (
         <div>
             <Navbar />
             <p>Homepage</p>
+            <button onClick={() => setShowUploader(true)}>Add Post</button>
+            {
+                showUploader ?
+                    <div>
+                        <Uploader/>
+                        <button onClick={() => setShowUploader(false)}>Cancel</button>
+                    </div>
+                :
+                    <></>
+            }
         </div>
     )
 }
