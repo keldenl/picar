c71e995393de3d382863169821567d230a395a23
Eliza Huang <eliza.huang@alaskaair.com>
2022-08-27 16:42:26 -0700

    make modal prettier

--- src/components/Uploader/index.jsx
+++ src/components/Uploader/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { DEFAULT_POST_CONFIG, DEFAULT_FETCH_CONFIG } from '../../api/middlewareConfig';
 import { toBase64 } from '../../utils';
+import { BiImage, BiX, BiImageAdd } from "react-icons/bi";
 
 import './style.css';
 
@@ -9,6 +10,7 @@ export function Uploader({ }) {
     const [showUploader, setShowUploader] = useState(false);
     const [uploadImg, setUploadImg] = useState([]);
     const [isUploading, setIsUploading] = useState(false);
+    const [postDescription, setPostDescription] = useState("");
 
 
     const handleUploadImage = async (e) => {
@@ -21,11 +23,12 @@ export function Uploader({ }) {
     }
 
     const handlePostImage = () => {
+        console.log(postDescription);
         setIsUploading(true);
         fetch('http://localhost:9000/upload', {
             ...DEFAULT_POST_CONFIG,
             ...DEFAULT_FETCH_CONFIG,
-            body: JSON.stringify({ uploadImg })
+            body: JSON.stringify({ uploadImg, description: postDescription })
         })
             .then((res) => {
                 if (res.status >= 400) {
@@ -40,23 +43,52 @@ export function Uploader({ }) {
             }).catch((err) => {
                 console.log(err);
             });
+        exitImageUpload();
+    }
+
+    const exitImageUpload = () => {
+        setUploadImg([]);
+        setShowUploader(false);
     }
 
     return (
         <div>
-            <button onClick={() => setShowUploader(true)}>Add Post</button>
+            <div className="add-button" onClick={() => setShowUploader(true)}>
+                <BiImageAdd size="2em" />
+                <span>Upload photo</span>
+            </div>
             {
                 showUploader ?
-                    <div className="modal">
-                        <img alt='preview' style={{ maxWidth: 300 }} src={uploadImg} />
-                        <input type='file' onChange={handleUploadImage} />
-                        {uploadImg != null ? <button onClick={handlePostImage} disabled={isUploading}>{!isUploading ? 'Post' : 'Uploading'}</button> : undefined}
-                        {/* <button>Upload your image here</button> */}
-                        {/* <button onClick={() => setShowUploader(false)}>Cancel</button> */}
+                    <div>
+                        <div className="modal">
+                            <h2 className="modal-title">Post new photo</h2>
+                            <BiX size="1.75em" className="cancel-button" onClick={exitImageUpload}/>
+                            <hr className="rainbow"></hr>
+                            {
+                                uploadImg === null || uploadImg.length === 0 ?
+                                    <div className="image-selector">
+                                        <BiImage size="3em" className="image-icon"/>
+                                        <input className="file-input" type='file' onChange={handleUploadImage} />
+                                    </div>
+                                :
+                                    <div className="image-uploader">
+                                        <div className="image-info">
+                                            <img alt='preview' style={{ maxWidth: 300 }} src={uploadImg} />
+                                            <div>
+                                                <p>Write a caption</p>
+                                                <textarea value={postDescription} onChange={e => setPostDescription(e.target.value)} />
+                                            </div>
+                                        </div>
+                                        <button onClick={handlePostImage} disabled={isUploading}>{!isUploading ? 'Post' : 'Uploading'}</button>
+                                    </div>
+                            }
+                        </div>
+                        <div className='overlay' onClick={exitImageUpload}>
+                        </div>
                     </div>
                     :
                     undefined
             }
         </div>
-    )
+    );
 }
